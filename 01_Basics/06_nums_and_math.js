8ad704f2327e97b981a7b70305854d63082342d5
Archit <archit@Archits-MacBook-Air.local>
2026-03-13 23:58:15 +0530

    Added nums and maths

---
+++ 01_Basics/06_nums_and_math.js
@@ -0,0 +1,14 @@
+const score = 400
+let balance = new Number(1000000)
+console.log(balance)
+console.log(balance.toString())
+console.log(balance.toFixed(2))
+console.log(balance.toLocaleString("en-IN"))
+
+
+/******* Maths ******/
+console.log(Math.abs(-3))
+console.log(Math.floor(6.6))
+console.log(Math.ceil(34.23))
+console.log(Math.round(3.4))
+console.log("Random value is", Math.round(Math.random()*100))
