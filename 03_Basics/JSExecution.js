fbd49bf4341c0236ee762642ede2b70f783894c2
Archit <archit@Archits-MacBook-Air.local>
2026-03-15 00:27:18 +0530

    JS EC

---
+++ 03_Basics/JSExecution.js
@@ -0,0 +1,12 @@
+/**
+ * JS has 2 types of execution contexts
+ * - Global execution context
+ * - Functional execution context
+ * 
+ * Working and execution of JS files
+ * Phase 01: Global execution => global object is kept in this
+ * Phase 02: Memory allocation => allocated memory for all the varibles
+ * Phase 03: Execution => values assigned to allocated memory
+ * Everytime a new execution context is created for function definition and then the result is returned to the global execution context
+
+ */
