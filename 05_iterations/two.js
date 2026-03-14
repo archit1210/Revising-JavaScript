2497694d9846312259c19fdec09c7b6b1b047643
Archit <archit@Archits-MacBook-Air.local>
2026-03-15 01:38:05 +0530

    while loops done

---
+++ 05_iterations/two.js
@@ -0,0 +1,14 @@
+let i = 0
+while(i <= 10) {
+    console.log(i)
+    i += 2
+}
+
+
+let score = 11
+do {
+    console.log(score)
+    score+=1;
+}while(score <= 10)
+    
+console.log("do while loop ended")
